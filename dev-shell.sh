0f2e0d2b709bed4b45345d28883c9e73772d1c21
Nick Spain <nicholas.spain96@gmail.com>
2018-12-31 19:47:13 +1100

    Move dockerfile to its own directory and reflect this in script

--- dev-shell.sh
+++ dev-shell.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Run PowerShell in an Ubuntu docker image
 DOCKER_IMAGE_TAG="nick/ps"
-docker build --froce-rm -t $DOCKER_IMAGE_TAG
+docker build --force-rm -t $DOCKER_IMAGE_TAG docker/
 docker run -it --rm -v "$(pwd)/PoshLastPass":/app $DOCKER_IMAGE_TAG
