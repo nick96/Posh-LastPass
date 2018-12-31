a0c21f2e67b98eff3d20df01d08dced0af73c007
Nick Spain <nicholas.spain96@gmail.com>
2018-12-31 20:33:02 +1100

    Fixup dev-shell script

--- dev-shell.sh
+++ dev-shell.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Run PowerShell in an Ubuntu docker image
 DOCKER_IMAGE_TAG="nick/ps"
-docker build --force-rm -t $DOCKER_IMAGE_TAG docker/
+docker build --force-rm -t $DOCKER_IMAGE_TAG -f docker/Dockerfile .
 docker run -it --rm -v "$(pwd)/PoshLastPass":/app $DOCKER_IMAGE_TAG
